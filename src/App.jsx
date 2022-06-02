22c0f395050aaf13b6989c95998368e196559612
Alan Gardner <urfolomeus@gmail.com>
2022-06-02 10:34:14 +0100

    Adding in a basic comparison

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import logo from "./logo.svg";
 
+import A11yWrapper from "./components/a11y/Wrapper";
+import AsIsWrapper from "./components/as_is/Wrapper";
 import GeneralNotes from "./components/GeneralNotes";
 
 function App() {
@@ -9,7 +11,14 @@ function App() {
       <header>
         <h1>Accessibility Testing Sandbox</h1>
       </header>
+
       <GeneralNotes />
+
+      <section className="comparison">
+        <AsIsWrapper />
+        <A11yWrapper />
+      </section>
+
       <div className="logoContainer">
         <img src={logo} className="logo" alt="logo" />
       </div>
---
+++ src/components/as_is/Wrapper.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import logo from "../../TXI-Icon-ColorOnLight-RGB.png";
+
+export default function Wrapper() {
+  return (
+    <div className="wrapper">
+      <div className="article">
+        <h1>I wrap the page!</h1>
+        <h3>But I've taken no heed of Accessibility Guidelines</h3>
+
+        <img src={logo} />
+
+        <p>
+          After walking some time in this way, the two ladies in front, the two
+          gentlemen behind, on resuming their places, after descending to the
+          brink of the river for the better inspection of some curious
+          water-plant, there chanced to be a little alteration. It originated in
+          Mrs. Gardiner, who, fatigued by the exercise of the morning, found
+          Elizabeth's arm inadequate to her support, and consequently preferred
+          her husband's. Mr. Darcy took her place by her niece, and they walked
+          on together. After a short silence, the lady first spoke. She wished
+          him to know that she had been assured of his absence before she came
+          to the place, and accordingly began by observing, that his arrival had
+          been very unexpected--"for your housekeeper," she added, "informed us
+          that you would certainly not be here till to-morrow; and indeed,
+          before we left Bakewell, we understood that you were not immediately
+          expected in the country." He acknowledged the truth of it all, and
+          said that business with his steward had occasioned his coming forward
+          a few hours before the rest of the party with whom he had been
+          travelling. "They will join me early to-morrow," he continued, "and
+          among them are some who will claim an acquaintance with you--Mr.
+          Bingley and his sisters."
+        </p>
+        <p>
+          Elizabeth answered only by a slight bow. Her thoughts were instantly
+          driven back to the time when Mr. Bingley's name had been the last
+          mentioned between them; and, if she might judge by his complexion, his
+          mind was not very differently engaged.
+        </p>
+        <p>
+          "There is also one other person in the party," he continued after a
+          pause, "who more particularly wishes to be known to you. Will you
+          allow me, or do I ask too much, to introduce my sister to your
+          acquaintance during your stay at Lambton?"
+        </p>
+      </div>
+    </div>
+  );
+}
