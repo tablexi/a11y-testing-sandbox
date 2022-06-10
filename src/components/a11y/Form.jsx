20d8bb5c94d0be751bc479713ec142fe9772bdb1
Alan Gardner <urfolomeus@gmail.com>
2022-06-10 12:21:16 +0100

    Form styling

--- src/components/a11y/Form.jsx
+++ src/components/a11y/Form.jsx
@@ -3,7 +3,7 @@ import { Formik, Field, Form } from "formik";
 
 export default function CompetitionForm() {
   return (
-    <div className="form">
+    <div className="competition">
       <h3>Competition</h3>
 
       <Formik
@@ -23,44 +23,41 @@ export default function CompetitionForm() {
         }) => (
           <Form>
             <div className="field">
-              <label htmlFor="email-field">Email</label>
+              <fieldset role="group">
+                <legend>
+                  It is a truth universally acknowledged, that a single man in
+                  possession of a good fortune, must be in want of ...
+                </legend>
+
+                <label className="radio-button">
+                  <Field type="radio" name="answer" value="a banana" />a banana
+                </label>
+
+                <label className="radio-button">
+                  <Field type="radio" name="answer" value="a wife" />a wife
+                </label>
+
+                <label className="radio-button">
+                  <Field type="radio" name="answer" value="an Aria Role" />
+                  an Aria Role
+                </label>
+              </fieldset>
+            </div>
+
+            <div className="field">
+              <label htmlFor="email-field">Please enter your email</label>
               <input
                 type="email"
                 id="email-field"
                 name="email"
                 onChange={handleChange}
                 onBlur={handleBlur}
-                placeholder="Enter your email..."
                 value={values.email}
               />
               {errors.email && touched.email && errors.email}
             </div>
 
-            <div className="field">
-              <div id="my-radio-group">Question</div>
-
-              <p>
-                It is a truth universally acknowledged, that a single man in
-                possession of a good fortune, must be in want of ...
-              </p>
-
-              <fieldset role="group" aria-labelledby="my-radio-group">
-                <legend>Your answer:</legend>
-
-                <label>
-                  <Field type="radio" name="answer" value="a banana" />a banana
-                </label>
-
-                <label>
-                  <Field type="radio" name="answer" value="a wife" />a wife
-                </label>
-
-                <label>
-                  <Field type="radio" name="answer" value="an Aria Role" />
-                  an Aria Role
-                </label>
-              </fieldset>
-
+            <div className="control">
               <button type="submit" disabled={isSubmitting}>
                 Submit
               </button>
--- src/components/as_is/Form.jsx
+++ src/components/as_is/Form.jsx
@@ -3,7 +3,7 @@ import { Formik, Field, Form } from "formik";
 
 export default function CompetitionForm() {
   return (
-    <div className="form">
+    <div className="competition">
       <h3>Competition</h3>
 
       <Formik
@@ -23,9 +23,33 @@ export default function CompetitionForm() {
         }) => (
           <Form>
             <div className="field">
+              <div class="question">
+                It is a truth universally acknowledged, that a single man in
+                possession of a good fortune, must be in want of ...
+              </div>
+
+              <div>
+                <div className="radio-button">
+                  <Field type="radio" name="answer" value="a banana" />
+                  <label>a banana</label>
+                </div>
+
+                <div className="radio-button">
+                  <Field type="radio" name="answer" value="a wife" />
+                  <label>a wife</label>
+                </div>
+
+                <div className="radio-button">
+                  <Field type="radio" name="answer" value="an Aria Role" />
+                  <label>an Aria Role</label>
+                </div>
+              </div>
+            </div>
+
+            <div className="field">
               <input
                 type="email"
-                id="email-field"
+                id="another-email-field"
                 name="email"
                 onChange={handleChange}
                 onBlur={handleBlur}
@@ -35,25 +59,7 @@ export default function CompetitionForm() {
               {errors.email && touched.email && errors.email}
             </div>
 
-            <div className="field">
-              <div id="my-radio-group">Question</div>
-
-              <p>
-                It is a truth universally acknowledged, that a single man in
-                possession of a good fortune, must be in want of ...
-              </p>
-
-              <div>
-                <label>a banana</label>
-                <Field type="radio" name="answer" value="a banana" />
-
-                <label>a wife</label>
-                <Field type="radio" name="answer" value="a wife" />
-
-                <label>an Aria Role</label>
-                <Field type="radio" name="answer" value="an Aria Role" />
-              </div>
-
+            <div className="control">
               <button type="submit" disabled={isSubmitting}>
                 Submit
               </button>
